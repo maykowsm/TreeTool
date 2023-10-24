1e1f8e8cee5aa659032f7d1aea01cd7da816dbb1
maykowsm <eng.maykowmenezes@gmail.com>
2023-10-24 17:28:48 -0300

    implementa geração de folhas poligonais

--- arvore.py
+++ arvore.py
@@ -1,26 +1,58 @@
-import Draft, FreeCAD, math, random, Part,Arch, FreeCADGui
+import Draft, FreeCAD, math, random, Part,Arch, FreeCADGui, Mesh
 
 
 interacao = 2				#Número de vezes que o algoritimo irá rodar (aumenta o numero de galhos)
 
-diametro_tronco = 400		#Diâmetro inicial do tronco da vegetação
-taxa_reducao_tronco = 0.850 #taxa na qual os galhos reduzem seu diametro
+diametro_tronco = 600		#Diâmetro inicial do tronco da vegetação
+taxa_reducao_tronco = 0.7 #taxa na qual os galhos reduzem seu diametro
 
-tamanho_tronco = 5000		#Altura do tronco inicial (influencia na altura e numero de galhos)
+tamanho_tronco = 2000		#Altura do tronco inicial (influencia na altura e numero de galhos)
+inclinação_max_tronco = 200	#desvio máximo do tronco inicial em relação a vertical
+
 tam_max_ramos = 100			#Tamanho minimo dos galhos (influencia na quantidade de galhos)(controle sensível)
-
 taxa_maxima_ramos = 70		#Taxa máxima de redução do tamanho dos galhos (influencia na quantidade de galhos e folhas)(controle sensível)
 taxa_minima_ramos = 30		#Taxa minima de redução do tamanho dos galhos (influencia na quantidade de galhos e folhas)(controle sensível)
 
 angulo_minimo = 40			#Ângulo minimo entre os galhos (infliencia na forma da copa)
 angulo_maximo = 60			#Ângulo máximo entre os galhos (infliencia na forma da copa)
-
 angulo_torcao = 80			#Angulo de torção dos galhos (infliencia na forma da copa)
 
 tam_min_folhas = 600		#Tamanho minimo das folhas
 tam_max_folhas = 1500		#Tamanho máximo das folhas
+tipo_folhas = 2				#1 - folhas esfericas lisas; 2 - folhas esfericas multifacetadas
 
-inclinação_max_tronco = 500	#desvio máximo do tronco inicial em relação a vertical
+
+
+
+
+def folhasEsfericas(pos):
+	esfera = App.ActiveDocument.addObject("Part::Sphere","folha")
+	esfera.Radius = random.randint(tam_min_folhas, tam_max_folhas)
+	esfera.Placement.Base = FreeCAD.Vector(pos)
+	esfera.ViewObject.DisplayMode = 'Shaded'
+	FreeCAD.ActiveDocument.recompute()
+
+def folhasFacetadas(pos):
+	#cria a malha
+	malha = App.ActiveDocument.addObject("Mesh::Sphere","malha")
+	malha.Radius= random.randint(tam_min_folhas, tam_max_folhas)
+	malha.Sampling= 5
+	malha.Placement.Base = FreeCAD.Vector(pos)
+	FreeCAD.ActiveDocument.recompute()
+	
+	#criando o solido das folhas
+	esfera = FreeCAD.ActiveDocument.addObject('Part::Feature', 'folha')
+	forma = Part.Shape()
+	forma.makeShapeFromMesh(malha.Mesh.Topology, 0.1)
+	esfera.Shape = forma
+	esfera.purgeTouched()
+	FreeCAD.ActiveDocument.recompute()
+
+
+	#Apaga a malha criada
+	FreeCAD.ActiveDocument.removeObject(malha.Name)
+	del(forma)
+	FreeCAD.ActiveDocument.recompute()
 
 
 
@@ -33,7 +65,7 @@ def ramo(comprimento, angulo, point, diametro):
 		ang2 = (random.randint(-angulo_torcao,angulo_torcao) * math.pi)/180
 		p2 = FreeCAD.Vector((comprimento*math.sin(angulo_rad)+p1.x,comprimento*math.sin(ang2)+p1.y, comprimento*math.cos(angulo_rad)+p1.z))
 		linha = Draft.make_wire([p1,p2], closed=False, face=False, support=None)
-		linha.Label = 'linha'
+		linha.Label = 'rascunho'
 		tubo = Arch.makePipe(linha)
 		tubo.Diameter = diametro * taxa_reducao_tronco
 		tubo.Label = 'ramo'
@@ -50,7 +82,7 @@ def ramo(comprimento, angulo, point, diametro):
 		ang2 = (random.randint(-angulo_torcao,angulo_torcao) * math.pi)/180
 		p3 = FreeCAD.Vector((comprimento*math.sin(novo_angulo)+p1.x,comprimento*math.sin(ang2)+p1.y, comprimento*math.cos(novo_angulo)+p1.z))
 		linha = Draft.make_wire([p1,p3], closed=False, face=False, support=None)
-		linha.Label = 'linha'
+		linha.Label = 'rascunho'
 		tubo = Arch.makePipe(linha)
 		tubo.Diameter = diametro
 		tubo.Label = 'ramo'
@@ -66,22 +98,22 @@ def ramo(comprimento, angulo, point, diametro):
 		ramo(novo_comprimento, novo_angulo, (p3.x, p3.y, p3.z), diametro)
 
 	else:
-		pass
-		esfera = App.ActiveDocument.addObject("Part::Sphere","folha")
-		esfera.Radius = random.randint(tam_min_folhas, tam_max_folhas)
-		esfera.Placement.Base = FreeCAD.Vector(point)
-		esfera.ViewObject.DisplayMode = 'Shaded'
-		FreeCAD.ActiveDocument.recompute()
+		if tipo_folhas == 1:
+			folhasEsfericas(point)
+		elif tipo_folhas == 2:
+			folhasFacetadas(point)
 
-
+#Cria os pontos para o tronco
 p1 = FreeCAD.Vector(0,0,0)
 p2 = FreeCAD.Vector(random.randint(-inclinação_max_tronco,inclinação_max_tronco),random.randint(-inclinação_max_tronco,inclinação_max_tronco), tamanho_tronco)
 
+#intera para criar os galhos
 for i in range(interacao):
 	ramo(tamanho_tronco,0,p2, diametro_tronco)
 
+#cria o tronco
 linha = Draft.make_wire([p1,p2], closed=False, face=False, support=None)
-linha.Label = 'linha'
+linha.Label = 'rascunho'
 tubo = Arch.makePipe(linha)
 tubo.Diameter = diametro_tronco
 tubo.Label = 'ramo'
@@ -90,7 +122,7 @@ tubo.Label = 'ramo'
 lista_objetos = FreeCAD.ActiveDocument.Objects
 lista_ramos = list(filter(lambda obj: 'ramo' in obj.Label, lista_objetos))
 lista_folhas = list(filter(lambda obj: 'folha' in obj.Label, lista_objetos))
-lista_linhas = list(filter(lambda obj: 'linha' in obj.Label, lista_objetos))
+lista_linhas = list(filter(lambda obj: 'rascunho' in obj.Label, lista_objetos))
 
 compound_ramos = App.activeDocument().addObject("Part::Compound","compound_ramos")
 compound_ramos.Links = lista_ramos
@@ -132,5 +164,6 @@ for linha in lista_linhas:
 FreeCAD.ActiveDocument.recompute()
 compound_ramos = App.activeDocument().addObject("Part::Compound","arvore")
 compound_ramos.Links = [copa, galhos]
-
 FreeCAD.ActiveDocument.recompute()
+compound_ramos.ViewObject.LineWidth = 1
+compound_ramos.ViewObject.PointSize = 1
